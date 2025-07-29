de494a72ad40fc6a5771e64347263547a9ed594a
Wolf Lycan <124433492+WolfLycanorcant@users.noreply.github.com>
2025-07-29 10:43:01 +0800

    Update ENV.bat

--- ENV.bat
+++ ENV.bat
@@ -1,3 +1,5 @@
+#If the environment is the default environemnt created, as venv, double clicking and running this on a windows machine will enter the environment with the python dependencies required. 
+
 python -m venv venv
 CALL .\venv\Scripts\activate
 cmd
